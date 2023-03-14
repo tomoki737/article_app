a021862b6ea5242017a2f69a78328d32db391993
tomoki737 <tomoki.k2159@gmail.com>
2023-03-14 14:55:49 +0900

    First commit

--- Makefile
+++ Makefile
@@ -8,9 +8,13 @@ build:
 	docker-compose build --no-cache --force-rm
 ps:
 	docker-compose ps
-app:
-	docker-compose exec -it app sh
+go:
+	docker-compose exec -it back sh
+vue:
+	docker-compose exec -it front bash
 db:
 	docker-compose exec -it db bash
 sql:
 	docker compose exec db bash -c 'mysql -u $$MYSQL_USER -p$$MYSQL_PASSWORD $$MYSQL_DATABASE'
+dev:
+	docker compose exec front bash -c 'npm run dev'
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"net/http"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"app/database"
+)
+
+type Article struct {
+	Title string
+	Body  string
+}
+
+func index(w http.ResponseWriter, r *http.Request)  {
+	rows, err := db.Query("SELECT title, body FROM articles")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		article := &Article{}
+		if err := rows.Scan(&article.Title, &article.Body); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(article)
+	}
+}
+
+func main() {
+	db := database.GetDB()
+	http.HandleFunc("/aricles", index)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
